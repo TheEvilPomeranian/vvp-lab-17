ec90937ddf78e9fbe79e9f7dce42c269421cad7b
TheEvilPomeranian <90762280+TheEvilPomeranian@users.noreply.github.com>
2022-01-12 00:48:37 +0300

    Create VVP-lab-17-3.cpp

---
+++ VVP-lab-17-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	cout << "Задача 3. \nДан массив A размера N. \nНайти минимальный элемент из его элементов с четными номерами: A2, A4, A6..." << endl;
+	int N, t = 0, rem;
+	cout << "Введите значение N: "; cin >> N;
+	int* arr = new int[N];
+	cout << "Введите значения для каждого элемента массива, избегая повтора:" << endl;
+	for (int i = 0; i < N; i++) {
+		cout << "arr[" << i << "] = "; cin >> arr[i];
+
+	}
+	cout << "Элементы с четными номерами: " << endl;
+	for (int i = 0; i < N; i++) {
+		if (i % 2 == 0) {
+			cout << "arr[" << i << "] = " << arr[i] << endl;
+			if (arr[i] < t) {
+				rem = arr[i];
+				t = arr[i];
+			}
+		}
+
+	}
+	cout << "Минимальный элементс с четным номером:" << rem <<  endl;
+}
